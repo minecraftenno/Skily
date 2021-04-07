94ce69405ead388e2bfdf45ce114328b0639b332
minecraftenno <68814721+minecraftenno@users.noreply.github.com>
2021-04-07 20:45:47 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,2 +1,4 @@
-node index.js
+@echo off
+echo Le bot se demarre sans extensions, si vous voulez utiliser une extension, allez dans le fichier extensions et choissisez le module a démarrer et démarrer le antiraid.bat
 pause
+node index.js
